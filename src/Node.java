d32e3d18d6011e218b36c604e9cb3c22ec3df65d
nicoalbo90 <nicoalbo90@gmail.com>
2010-04-12 20:18:03 +0000

    Aplicada Genericidad

--- src/Node.java
+++ src/Node.java
@@ -1,19 +1,19 @@
 /** Node of a singly linked list of strings. */
-public class Node {
-  private String element;	// we assume elements are character strings
-  private Node next;
+public class Node<E> implements Position<E> {
+  private E element;	// we assume elements are character strings
+  private Node<E> next;
   /** Creates a node with the given element and next node. */
-  public Node(String s, Node n) {
-    element = s;
+  public Node(E e, Node<E> n) {
+    element = e;
     next = n;
   }
   /** Returns the element of this node. */
-  public String getElement() { return element; }
+  public E element() { return element; }
   /** Returns the next node of this node. */
-  public Node getNext() { return next; }
+  public Node<E> getNext() { return next; }
   // Modifier methods:
   /** Sets the element of this node. */
-  public void setElement(String newElem) { element = newElem; }
+  public void setElement(E newElem) { element = newElem; }
   /** Sets the next node of this node. */
-  public void setNext(Node newNext) { next = newNext; }
+  public void setNext(Node<E> newNext) { next = newNext; }
 }
