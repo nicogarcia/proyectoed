6e5fc9016a4f95e442ddecef8812e09b76759841
schiaffinomartin@gmail.com <schiaffinomartin@gmail.com@bca731e3-6e8d-8941-7ef9-e8c2e2188fc1>
2010-04-12 20:44:31 +0000

    Última actualización!

--- trunk/src/InvalidPositionException.java
+++ trunk/src/InvalidPositionException.java
@@ -1,7 +1,6 @@
-
 public class InvalidPositionException extends Exception {
 
-		public InvalidPositionException (String msg) {
-			super (msg);
-		}
+	public InvalidPositionException(String msg) {
+		super(msg);
+	}
 }
--- trunk/src/Lista.java
+++ trunk/src/Lista.java
@@ -8,8 +8,21 @@ public class Lista<E> implements PositionList<E> {
 	private Position<E> current;
 	private int size;
 
+	/**
+	 * Devuelve el tamaño de la lista
+	 */
+	public int size() {
+		return size;
+	}
+
 	public boolean isEmpty() {
 		return size == 0;
+	}
+
+	public Position<E> first() throws EmptyListException {
+		if (isEmpty())
+			throw new EmptyListException("Lista:: first() :List is empty");
+		return head;
 	}
 
 	public Position<E> last() throws EmptyListException {
