7de610b7b5d436c78cb2b778ecc95163b19ae96f
schiaffinomartin@gmail.com <schiaffinomartin@gmail.com@bca731e3-6e8d-8941-7ef9-e8c2e2188fc1>
2010-05-14 00:08:06 +0000

    Implementación TDAPila

---
+++ trunk/src/TDAPila/Node.java
@@ -0,0 +1,33 @@
+package TDAPila;
+
+public class Node<E> {
+	private E element; 
+	private Node<E> next;
+
+	/** Creates a node with the given element and next node. */
+	public Node(E e, Node<E> n) {
+		element = e;
+		next = n;
+	}
+
+	/** Returns the element of this node. */
+	public E element() {
+		return element;
+	}
+
+	/** Returns the next node of this node. */
+	public Node<E> getNext() {
+		return next;
+	}
+
+	// Modifier methods:
+	/** Sets the element of this node. */
+	public void setElement(E newElem) {
+		element = newElem;
+	}
+
+	/** Sets the next node of this node. */
+	public void setNext(Node<E> newNext) {
+		next = newNext;
+	}
+}
