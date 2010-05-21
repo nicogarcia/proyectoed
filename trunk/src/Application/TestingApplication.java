ed87caf4bae3c8dc1459c3a94d3285723759d456
nicoalbo90 <nicoalbo90@bca731e3-6e8d-8941-7ef9-e8c2e2188fc1>
2010-05-21 00:21:02 +0000

    Punto F hasta el 5

--- trunk/src/Application/TestingApplication.java
+++ trunk/src/Application/TestingApplication.java
@@ -4,20 +4,20 @@ import GeneralTree.Arbol;
 import TDALista.Position;
 
 public class TestingApplication {
-	
+
 	public static void main(String[] args) {
 		cargarArbol('A');
-		//System.out.println(printPreOrder());
+		// System.out.println(printPreOrder());
 		agregarNodo('B', 'A');
-		agregarNodo('c', 'B');
+		agregarNodo('C', 'B');
 		agregarNodo('D', 'A');
 		agregarNodo('E', 'A');
-		agregarNodo('F','E');
-		agregarNodo('G','E');
-		System.out.println(printPreOrder());
+		agregarNodo('F', 'E');
+		agregarNodo('G', 'E');
+		System.out.println(printNiveles());
+		// System.out.println(printPosOrder());
 	}
-	
-	
+
 	private static Arbol<Character> miArbol;
 
 	public static void cargarArbol(Character rotuloRaiz) {
@@ -27,24 +27,34 @@ public class TestingApplication {
 	public static void agregarNodo(Character rotulo, Character rPadre) {
 		miArbol.insertar(rotulo, rPadre);
 	}
-	
-	//FIXME MODIFICAR PARA HACER EL DIBUJITO DE NICO
+
+	// FIXME MODIFICAR PARA HACER EL DIBUJITO DE NICO
 	public static String printPreOrder() {
 		String ret = "[ ";
-		for (Position<Character> pos :miArbol.preOrderPositions())
-			ret+=pos.element()+", ";
-		ret = ret.substring(0, ret.length()-2)+" ]";
+		for (Position<Character> pos : miArbol.preOrderPositions())
+			ret += pos.element() + ", ";
+		ret = ret.substring(0, ret.length() - 2) + " ]";
 		return ret;
 	}
-	
-	public String printPosOrder() {
+
+	public static String printPosOrder() {
 		String ret = "[ ";
-		for (Position<Character> pos :miArbol.posOrderPositions())
-			ret+=pos.element()+", ";
-		ret = ret.substring(0, ret.length()-2)+" ]";
+		for (Position<Character> pos : miArbol.posOrderPositions())
+			ret += pos.element() + ", ";
+		ret = ret.substring(0, ret.length() - 2) + " ]";
 		return ret;
 	}
-	
-	
-	
+
+	public static String printNiveles() {
+		String ret = "[ ";
+		for (Character pos : miArbol.listadoNiveles()) {
+			if (pos == null)
+				ret += "\n";
+			else
+				ret += pos + ", ";
+		}
+		ret = ret.substring(0, ret.length() - 2) + " ]";
+		return ret;
+	}
+
 }
