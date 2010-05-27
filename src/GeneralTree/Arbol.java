dd7fc23fd1b5b2b1f75bd790bd299aac9d9a0cc1
schiaffinomartin <schiaffinomartin@gmail.com>
2010-05-27 01:15:08 +0000

    Javadooocs!

--- src/GeneralTree/Arbol.java
+++ src/GeneralTree/Arbol.java
@@ -1,7 +1,5 @@
 package GeneralTree;
 
-//TODO Faltan javadocs. Agregar!
-
 import java.util.Iterator;
 
 import sun.font.EAttribute;
@@ -11,15 +9,44 @@ import TDACola.Cola;
 import TDALista.*;
 import TDAPila.Pila;
 
+// TODO: Auto-generated Javadoc
+/**
+ * Esta clase define el comportamiento de un arbol general con elementos
+ * genericos de tipo E.
+ * 
+ * @param <E>
+ *            Objeto genérico.
+ * @author Martin Schiaffino [93718]
+ * @author Nicolas Garcia [9307]
+ */
 public class Arbol<E> implements GeneralTree<E> {
 
+	// Atributos
+	/** The root. */
 	protected TNode<E> root;
+
+	/** The size. */
 	protected int size;
 
+	/**
+	 * Construye un arbol cuya raiz tiene como rotulo la recibida por parametro.
+	 * 
+	 * @param rotuloRaiz
+	 *            Rotulo de la raiz.
+	 */
 	public Arbol(E rotuloRaiz) {
 		root = new TNode<E>(null, rotuloRaiz);
 	}
 
+	/**
+	 * Inserta un nuevo nodo en el arbol.
+	 * 
+	 * @param rotulo
+	 *            Rotulo del nodo a insertar.
+	 * @param rPadre
+	 *            Rotulo del padre del nodo a insertar.
+	 * @return El nuevo nodo insertado.
+	 */
 	public TNode<E> insertar(E rotulo, E rPadre) {
 		for (Position<E> pos : positions())
 			if (pos.element() == rPadre) {
@@ -36,6 +63,15 @@ public class Arbol<E> implements GeneralTree<E> {
 		return null;
 	}
 
+	/**
+	 * Retorna una colección iterable de hijos de la posición recibida.
+	 * 
+	 * @param v
+	 *            Posición padre de los hijos de la lista.
+	 * @return Una colección iterable de hijos.
+	 * @throws InvalidPositionException
+	 *             the invalid position exception
+	 */
 	public Iterable<Position<E>> children(Position<E> v)
 			throws InvalidPositionException {
 		TNode<E> p = checkPosition(v);
@@ -46,6 +82,15 @@ public class Arbol<E> implements GeneralTree<E> {
 		return lista;
 	}
 
+	/**
+	 * Chequea si la posicion recibida es valida.
+	 * 
+	 * @param v
+	 *            Posicion a chequear.
+	 * @return Retorna la posicion casteada a nodo.
+	 * @throws InvalidPositionException
+	 *             En caso de que la posicion sea invalida.
+	 */
 	private TNode<E> checkPosition(Position<E> v)
 			throws InvalidPositionException {
 		if (v == null)
@@ -60,26 +105,55 @@ public class Arbol<E> implements GeneralTree<E> {
 		}
 	}
 
+	/**
+	 * Determina si la cola esta vacia o no.
+	 * 
+	 * @return true, if is empty
+	 */
 	public boolean isEmpty() {
 		return root == null;
 	}
 
+	/**
+	 * Determina si la posicion recibida corresponde a un nodo externo.
+	 * 
+	 * @param v
+	 *            Posicion a verificar.
+	 * @return true, if is external
+	 * @throws InvalidPositionException
+	 *             En caso de que la posicion sea invalida.
+	 */
 	public boolean isExternal(Position<E> v) throws InvalidPositionException {
 		TNode<E> nodo = checkPosition(v);
 		return (nodo.getChildren().isEmpty());
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see GeneralTree.GeneralTree#isInternal(TDALista.Position)
+	 */
 	public boolean isInternal(Position<E> v) throws InvalidPositionException {
 		TNode<E> nodo = checkPosition(v);
 		return (nodo.getChildren() != null);
 
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see GeneralTree.GeneralTree#isRoot(TDALista.Position)
+	 */
 	public boolean isRoot(Position<E> v) throws InvalidPositionException {
 		TNode<E> nodo = checkPosition(v);
 		return nodo == root;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see GeneralTree.GeneralTree#iterator()
+	 */
 	public Iterator<E> iterator() {
 		Lista<E> lista = new Lista<E>();
 		for (Position<E> p : positions())
@@ -87,34 +161,72 @@ public class Arbol<E> implements GeneralTree<E> {
 		return lista.iterator();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see GeneralTree.GeneralTree#parent(TDALista.Position)
+	 */
 	public Position<E> parent(Position<E> v) throws InvalidPositionException,
 			BoundaryViolationException {
 		TNode<E> nodo = checkPosition(v);
 		return nodo.getParent();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see GeneralTree.GeneralTree#positions()
+	 */
 	public Iterable<Position<E>> positions() {
 		return preOrderPositions();
 	}
 
 	// FIXME PREGUNTAR SI SE PUEDE HACER UN POSITIONPRE Y OTRO POSITIONPOS
+	/**
+	 * Pre order positions.
+	 * 
+	 * @return the iterable
+	 */
 	public Iterable<Position<E>> preOrderPositions() {
 		Lista<Position<E>> lista = new Lista<Position<E>>();
 		preOrder(lista, root);
 		return lista;
 	}
 
+	/**
+	 * Pos order positions.
+	 * 
+	 * @return the iterable
+	 */
 	public Iterable<Position<E>> posOrderPositions() {
 		Lista<Position<E>> lista = new Lista<Position<E>>();
 		posOrder(lista, root);
 		return lista;
 	}
 
+	/**
+	 * Remplaza el elemento del la posicion recibida.
+	 * 
+	 * @param v
+	 *            Posicion a la cual se le reemplaza el elemento.
+	 * 
+	 * @param e
+	 *            Nuevo elemento de la posicion.
+	 * 
+	 * @return El elemento anterior de la posicion.
+	 */
 	public E replace(Position<E> v, E e) throws InvalidPositionException {
 		TNode<E> nodo = checkPosition(v);
 		return nodo.setElement(e);
 	}
 
+	/**
+	 * Getter de la raiz.
+	 * 
+	 * @returns La raiz del arbol.
+	 * @throws EmptyTreeException
+	 *             En caso de que el arbol este vacio.
+	 */
 	public Position<E> root() throws EmptyTreeException {
 		if (isEmpty())
 			throw new EmptyTreeException(
@@ -122,9 +234,25 @@ public class Arbol<E> implements GeneralTree<E> {
 		return root;
 	}
 
+	/**
+	 * Getter del tamaño.
+	 * 
+	 * @return Tamaño del arbol.
+	 */
 	public int size() {
 		return size;
 	}
+
+	/**
+	 * Añade a una lista las positions recorriendo en pre-orden.
+	 * 
+	 * @param l
+	 *            Lista a la cual se añaden las positions.
+	 * 
+	 * @param n
+	 *            Raiz del arbol (cuando es llamado).
+	 * 
+	 */
 
 	private void preOrder(Lista<Position<E>> l, TNode<E> n) {
 		l.addLast(n);
@@ -132,12 +260,28 @@ public class Arbol<E> implements GeneralTree<E> {
 			preOrder(l, h);
 	}
 
+	/**
+	 * Añade a una lista las positions recorriendo en pos-orden.
+	 * 
+	 * @param l
+	 *            Lista a la cual se añaden las positions.
+	 * 
+	 * @param n
+	 *            Raiz del arbol (cuando es llamado).
+	 * 
+	 */
 	private void posOrder(Lista<Position<E>> l, TNode<E> n) {
 		for (TNode<E> h : n.getChildren())
 			posOrder(l, h);
 		l.addLast(n);
 	}
 
+	/**
+	 * Devuelve una lista con los elementos del arbol, con nulls que representan
+	 * saltos de nivel.
+	 * 
+	 * @return El listado de niveles.
+	 */
 	public Lista<E> listadoNiveles() {
 		Lista<E> lista = new Lista<E>();
 		Cola<TNode<E>> cola = new Cola<TNode<E>>();
@@ -165,6 +309,11 @@ public class Arbol<E> implements GeneralTree<E> {
 		return lista;
 	}
 
+	/**
+	 * Cuenta los niveles del arbol.
+	 * 
+	 * @return La cantidad de niveles del arbol.
+	 */
 	public int contarNiveles() {
 		int cant = 1;
 		for (E el : listadoNiveles())
@@ -174,6 +323,21 @@ public class Arbol<E> implements GeneralTree<E> {
 		return cant;
 	}
 
+	/**
+	 * Elimina un nodo del arbol.
+	 * 
+	 * @param p
+	 *            Position (nodo) a eliminar.
+	 * 
+	 * @return El elemento del nodo eliminado.
+	 * 
+	 * @throws InvalidPositionException
+	 *             En caso de que la posicion sea invalida.
+	 * 
+	 * @throws EmptyTreeException
+	 *             En caso de que el arbol este vacio.
+	 * 
+	 */
 	public E removeNode(Position<E> p) throws InvalidPositionException,
 			EmptyTreeException {
 		if (isEmpty())
@@ -217,10 +381,12 @@ public class Arbol<E> implements GeneralTree<E> {
 	}
 
 	/**
+	 * Crea una pila con los ancestros del nodo recibido.
 	 * 
-	 * @param rotulo
-	 *            Rotulo del elemento del que se desea obtener los ancestros,
-	 * @return Una cola con los ancestros del nodo con el rï¿½tulo recibido,
+	 * @param nodo
+	 *            Nodo de cual se buscan los ancestros.
+	 * 
+	 * @return Una pila con los ancestros del nodo.
 	 */
 	public Pila<E> ancestros(TNode<E> nodo) {
 		Pila<E> pila = new Pila<E>();
@@ -233,6 +399,14 @@ public class Arbol<E> implements GeneralTree<E> {
 		return pila;
 	}
 
+	/**
+	 * Busca el la posicion correspondiente al rotulo recibido.
+	 * 
+	 * @param rotulo
+	 *            the rotulo
+	 * @return La posicion encontrada o nulo en caso de que no haya ninguna con
+	 *         el rotulo recibido
+	 */
 	public Position<E> findNodo(E rotulo) {
 		Lista<Position<E>> lista = new Lista<Position<E>>();
 		preOrder(lista, root);
@@ -243,6 +417,17 @@ public class Arbol<E> implements GeneralTree<E> {
 	}
 
 	// FIXME cambiar nombre
+	/**
+	 * Find nodo piola.
+	 * 
+	 * @param rotulo
+	 *            the rotulo
+	 * @param inicio
+	 *            the inicio
+	 * @return the position
+	 * @throws InvalidPositionException
+	 *             the invalid position exception
+	 */
 	public Position<E> findNodoPiola(E rotulo, Position<E> inicio)
 			throws InvalidPositionException {
 		TNode<E> ini = checkPosition(inicio);
@@ -255,13 +440,25 @@ public class Arbol<E> implements GeneralTree<E> {
 		return ini;
 	}
 
-	public int height(Position<E> nodo) throws InvalidPositionException {
-		if (isExternal(nodo))
+	/**
+	 * Calcula la altura de la posicion recibido.
+	 * 
+	 * @param pos
+	 *            Posicion que se analiza.
+	 * 
+	 * @return La altura de la posicion.
+	 * 
+	 * @throws InvalidPositionException
+	 *             En caso de que la posicion sea invalida.
+	 * 
+	 */
+	public int height(Position<E> pos) throws InvalidPositionException {
+		if (isExternal(pos))
 			return 0;
 		int h = 0;
-		for (Position<E> pos : children(nodo))
-			h = Math.max(h, height(pos));
-		return 1+h;
+		for (Position<E> posit : children(pos))
+			h = Math.max(h, height(posit));
+		return 1 + h;
 	}
 
 }
